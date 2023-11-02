6cadd65c4f25e347901aa601a9e284620d810076
Jose Guerra <jose.guerra.m@uni.pe>
2023-11-01 19:45:18 -0500

    historias medicas
    api para historias medicas

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Project (models. Model):
-    title = models. CharField (max_length=200)
-    description = models.TextField()
-    technology = models.CharField (max_length=200)
-    created_at = models.DateTimeField (auto_now_add=True)
+    dni = models.CharField(max_length=8, null=True)
+    antecedentes_patologicos = models.CharField(max_length=200)
+    antecedentes_quirurgicos = models.CharField(max_length=200)
+    rams = models.CharField(max_length=200)
+    relato_cronologico = models.CharField(max_length=200)
+    plan = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
--- projects/serializers.py
+++ projects/serializers.py
@@ -4,5 +4,5 @@ from projects.models import Project
 class ProjectSerializer(serializers. ModelSerializer):
     class Meta:
         model = Project
-        fields = ('id', 'title', 'description', 'technology', 'created_at')
+        fields = ('id','dni', 'antecedentes_patologicos', 'antecedentes_quirurgicos', 'rams', 'relato_cronologico','plan', 'created_at')
         read_only_fields = ('created_at', )
